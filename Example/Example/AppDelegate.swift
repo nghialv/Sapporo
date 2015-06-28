148c2c5521e3653bc269297efc41701d475d06a2
nghialv <levan_nghia@cyberagent.co.jp>
2015-06-29 00:21:05 +0900

    setup project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Le VanNghia on 6/29/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Example/Sapporo/Sapporo.h
@@ -0,0 +1,19 @@
+//
+//  Sapporo.h
+//  Sapporo
+//
+//  Created by Le VanNghia on 6/29/15.
+//  Copyright (c) 2015 Le Van Nghia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sapporo.
+FOUNDATION_EXPORT double SapporoVersionNumber;
+
+//! Project version string for Sapporo.
+FOUNDATION_EXPORT const unsigned char SapporoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sapporo/PublicHeader.h>
+
+
